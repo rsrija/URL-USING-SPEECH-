c50bda5b85a2577efed7687d477369953f95cf3b
SRIJA <rsrija01@gmail.com>
2018-05-02 15:02:39 +0530

    Update open_chrome_url.py

--- open_chrome_url.py
+++ open_chrome_url.py
@@ -1,12 +1,16 @@
 import speech_recognition as sr
 import webbrowser as wb
-
+import pyttsx3
 chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
 
 r = sr.Recognizer()
+r.energy_threshold = 4000
+engine = pyttsx3.init()
 
 with sr.Microphone() as source:
     print('Say Something!')
+    engine.say('Hola! I would help you in redirecting you to the web page! Please give me the url')
+    engine.runAndWait()
     audio = r.listen(source)
     print('Done!')
 
@@ -14,7 +18,9 @@ try:
     text = r.recognize_google(audio)
     print('You said:\n' + text)
     wb.get(chrome_path).open(text)
-    print('WELCOME TO' + text)
+    print('Welcome to '+text)
+    engine.say('Welcome to '+text)
+    engine.runAndWait()
 
 except Exception as e:
     print(e)
